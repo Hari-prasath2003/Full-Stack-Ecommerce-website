cd4c9da8d967357e72d004001349e47e46b41bb7
Hari-prasath2003 <124680450+Hari-prasath2003@users.noreply.github.com>
2024-03-15 23:39:30 +0530

    Update ecommerce.service.ts
    v

--- src/app/services/ecommerce.service.ts
+++ src/app/services/ecommerce.service.ts
@@ -1,300 +1,301 @@
-import { Injectable } from '@angular/core';
-
-
-const signupURL='https://fullstackecommercewebsite.pythonanywhere.com/api/signup';
-const signinURL='https://fullstackecommercewebsite.pythonanywhere.com/api/signin';
-const productsURL='https://fullstackecommercewebsite.pythonanywhere.com/api/products';
-@Injectable({
-  providedIn: 'root'
-})
+// import { Injectable } from '@angular/core';
 
 
 
-export class EcommerceService {
+// const signupURL='https://fullstackecommercewebsite.pythonanywhere.com/api/signup';
+// const signinURL='https://fullstackecommercewebsite.pythonanywhere.com/api/signin';
+// const productsURL='https://fullstackecommercewebsite.pythonanywhere.com/api/products';
+// @Injectable({
+//   providedIn: 'root'
+// })
+
+
+
+// export class EcommerceService {
   
   
-  constructor() { }
+//   constructor() { }
   
-  fetchToken(){
-    return localStorage.getItem('token')
-  }
-  registrationService(data:any){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let options={
-      method:"POST",
-      headers:header,
-      body:JSON.stringify(data)
-    }
-    return fetch(signupURL,options)
-  }
+//   fetchToken(){
+//     return localStorage.getItem('token')
+//   }
+//   registrationService(data:any){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let options={
+//       method:"POST",
+//       headers:header,
+//       body:JSON.stringify(data)
+//     }
+//     return fetch(signupURL,options)
+//   }
   
-  signinService(data:any){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let options={
-      method:"POST",
-      body:JSON.stringify(data),
-      headers:header
-    }
-    return fetch(signinURL,options)
-  }
-  ProductsService(){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let options={
-      method:"GET",
-      headers:header
-    }
-    return fetch(productsURL,options)
-  }
-  productDetailService(id:any){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let options={
-      method:"GET",
-      headers:header
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/products/${id}`,options)
-  }
-  addToCartService(id:any){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let token =this.fetchToken()
-    if(token){
-      header.append('Authorization',token)
-    }
-    let options={
-      method:"POST",
-      headers:header
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/products/${id}/add_to_cart`,options)
-  }
-  addTowishlistService(id:any){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let token =this.fetchToken()
-    if(token){
-      header.append('Authorization',token)
-    }
-    let options={
-      method:"POST",
-      headers:header
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/products/${id}/add_to_wishlist`,options)
-  }
-  cartListservice(){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let token =this.fetchToken()
-    if(token){
-      header.append('Authorization',token)
-    }
-    let options={
-      method:"GET",
-      headers:header
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/cart`,options)
-  }
-  wishlistListservice(){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let token =this.fetchToken()
-    if(token){
-      header.append('Authorization',token)
-    }
-    let options={
-      method:"GET",
-      headers:header
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/wishlist`,options)
-  }
+//   signinService(data:any){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let options={
+//       method:"POST",
+//       body:JSON.stringify(data),
+//       headers:header
+//     }
+//     return fetch(signinURL,options)
+//   }
+//   ProductsService(){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let options={
+//       method:"GET",
+//       headers:header
+//     }
+//     return fetch(productsURL,options)
+//   }
+//   productDetailService(id:any){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let options={
+//       method:"GET",
+//       headers:header
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/products/${id}`,options)
+//   }
+//   addToCartService(id:any){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let token =this.fetchToken()
+//     if(token){
+//       header.append('Authorization',token)
+//     }
+//     let options={
+//       method:"POST",
+//       headers:header
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/products/${id}/add_to_cart`,options)
+//   }
+//   addTowishlistService(id:any){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let token =this.fetchToken()
+//     if(token){
+//       header.append('Authorization',token)
+//     }
+//     let options={
+//       method:"POST",
+//       headers:header
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/products/${id}/add_to_wishlist`,options)
+//   }
+//   cartListservice(){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let token =this.fetchToken()
+//     if(token){
+//       header.append('Authorization',token)
+//     }
+//     let options={
+//       method:"GET",
+//       headers:header
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/cart`,options)
+//   }
+//   wishlistListservice(){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let token =this.fetchToken()
+//     if(token){
+//       header.append('Authorization',token)
+//     }
+//     let options={
+//       method:"GET",
+//       headers:header
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/wishlist`,options)
+//   }
 
-  placeOrderService(id:any,data:any){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let token =this.fetchToken()
-    if(token){
-      header.append('Authorization',token)
-    }
-    let options={
-      method:"POST",
-      headers:header,
-      body:JSON.stringify(data)
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/products/${id}/place_order`,options)
-  }
-  OrderListService(){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let token =this.fetchToken()
-    if(token){
-      header.append('Authorization',token)
-    }
-    let options={
-      method:"GET",
-      headers:header
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/orders`,options)
-  }
-  orderCancelservice(id:any){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let token =this.fetchToken()
-    if(token){
-      header.append('Authorization',token)
-    }
-    let options={
-      method:"PUT",
-      headers:header
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/orders/${id}/cancel`,options)
-  }
-  removeFromCartService(id:any){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let token =this.fetchToken()
-    if(token){
-      header.append('Authorization',token)
-    }
-    let options={
-      method:"DELETE",
-      headers:header
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/cart/${id}`,options)
-  }
-  removeFromwishlistService(id:any){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let token =this.fetchToken()
-    if(token){
-      header.append('Authorization',token)
-    }
-    let options={
-      method:"DELETE",
-      headers:header
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/wishlist/${id}`,options)
-  }
-  categoryService(){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let options={
-      method:"GET",
-      headers:header
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/category`,options)
-  }
-  categoryDetailService(id:any){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let options={
-      method:"GET",
-      headers:header
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/category/${id}`,options)
-  }
-  addReviewService(id:any,data:any){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let token =this.fetchToken()
-    if(token){
-      header.append('Authorization',token)
-    }
-    let options={
-      method:"POST",
-      headers:header,
-      body:JSON.stringify(data)
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/products/${id}/add_review`,options)
-  }
-  addProductService(data:any,file:any){
+//   placeOrderService(id:any,data:any){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let token =this.fetchToken()
+//     if(token){
+//       header.append('Authorization',token)
+//     }
+//     let options={
+//       method:"POST",
+//       headers:header,
+//       body:JSON.stringify(data)
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/products/${id}/place_order`,options)
+//   }
+//   OrderListService(){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let token =this.fetchToken()
+//     if(token){
+//       header.append('Authorization',token)
+//     }
+//     let options={
+//       method:"GET",
+//       headers:header
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/orders`,options)
+//   }
+//   orderCancelservice(id:any){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let token =this.fetchToken()
+//     if(token){
+//       header.append('Authorization',token)
+//     }
+//     let options={
+//       method:"PUT",
+//       headers:header
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/orders/${id}/cancel`,options)
+//   }
+//   removeFromCartService(id:any){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let token =this.fetchToken()
+//     if(token){
+//       header.append('Authorization',token)
+//     }
+//     let options={
+//       method:"DELETE",
+//       headers:header
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/cart/${id}`,options)
+//   }
+//   removeFromwishlistService(id:any){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let token =this.fetchToken()
+//     if(token){
+//       header.append('Authorization',token)
+//     }
+//     let options={
+//       method:"DELETE",
+//       headers:header
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/wishlist/${id}`,options)
+//   }
+//   categoryService(){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let options={
+//       method:"GET",
+//       headers:header
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/category`,options)
+//   }
+//   categoryDetailService(id:any){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let options={
+//       method:"GET",
+//       headers:header
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/category/${id}`,options)
+//   }
+//   addReviewService(id:any,data:any){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let token =this.fetchToken()
+//     if(token){
+//       header.append('Authorization',token)
+//     }
+//     let options={
+//       method:"POST",
+//       headers:header,
+//       body:JSON.stringify(data)
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/products/${id}/add_review`,options)
+//   }
+//   addProductService(data:any,file:any){
    
 
-    var formdata=new FormData()
-    for ( const key of Object.keys(data) ) {
-      var value = data[key];
-      formdata.append(key,value);
-      }
-    formdata.append("image", file, file.name);
-    let header=new Headers
-    let token =this.fetchToken()
-    if(token){
-      header.append('Authorization',token)
-    }
+//     var formdata=new FormData()
+//     for ( const key of Object.keys(data) ) {
+//       var value = data[key];
+//       formdata.append(key,value);
+//       }
+//     formdata.append("image", file, file.name);
+//     let header=new Headers
+//     let token =this.fetchToken()
+//     if(token){
+//       header.append('Authorization',token)
+//     }
     
-    let options={
-      method:"POST",
-      headers:header,
-      body:formdata
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/products/add`,options)
-  }
-  productRemoveService(id:any){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let token =this.fetchToken()
-    if(token){
-      header.append('Authorization',token)
-    }
-    let options={
-      method:"DELETE",
-      headers:header
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/products/${id}/remove`,options)
-  }
-  productEditService(id:any,data:any,file:any){
-    let header=new Headers
+//     let options={
+//       method:"POST",
+//       headers:header,
+//       body:formdata
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/products/add`,options)
+//   }
+//   productRemoveService(id:any){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let token =this.fetchToken()
+//     if(token){
+//       header.append('Authorization',token)
+//     }
+//     let options={
+//       method:"DELETE",
+//       headers:header
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/products/${id}/remove`,options)
+//   }
+//   productEditService(id:any,data:any,file:any){
+//     let header=new Headers
     
-    let token =this.fetchToken()
-    if(token){
-      header.append('Authorization',token)
-    }
-    var formdata=new FormData()
-    for ( const key of Object.keys(data) ) {
-      var value = data[key];
-      formdata.append(key,value);
-      }
-    formdata.append("image", file, file.name);
-    let options={
-      method:"PUT",
-      headers:header,
-      body:formdata
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/products/${id}/edit`,options)
-  }
+//     let token =this.fetchToken()
+//     if(token){
+//       header.append('Authorization',token)
+//     }
+//     var formdata=new FormData()
+//     for ( const key of Object.keys(data) ) {
+//       var value = data[key];
+//       formdata.append(key,value);
+//       }
+//     formdata.append("image", file, file.name);
+//     let options={
+//       method:"PUT",
+//       headers:header,
+//       body:formdata
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/products/${id}/edit`,options)
+//   }
 
-  categoryAddService(data:any,file:any){
-    let header=new Headers
+//   categoryAddService(data:any,file:any){
+//     let header=new Headers
     
-    let token =this.fetchToken()
-    if(token){
-      header.append('Authorization',token)
-    }
-    var formdata=new FormData()
-    for ( const key of Object.keys(data) ) {
-      var value = data[key];
-      formdata.append(key,value);
-      }
-    formdata.append("icon", file, file.name);
-    let options={
-      method:"POST",
-      headers:header,
-      body:formdata
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/category/add`,options)
-  }
-  categoryRemoveService(id:any){
-    let header=new Headers
-    header.append('Content-Type','application/json')
-    let token =this.fetchToken()
-    if(token){
-      header.append('Authorization',token)
-    }
-    let options={
-      method:"DELETE",
-      headers:header
-    }
-    return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/category/${id}/remove`,options)
-  }
-}
+//     let token =this.fetchToken()
+//     if(token){
+//       header.append('Authorization',token)
+//     }
+//     var formdata=new FormData()
+//     for ( const key of Object.keys(data) ) {
+//       var value = data[key];
+//       formdata.append(key,value);
+//       }
+//     formdata.append("icon", file, file.name);
+//     let options={
+//       method:"POST",
+//       headers:header,
+//       body:formdata
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/category/add`,options)
+//   }
+//   categoryRemoveService(id:any){
+//     let header=new Headers
+//     header.append('Content-Type','application/json')
+//     let token =this.fetchToken()
+//     if(token){
+//       header.append('Authorization',token)
+//     }
+//     let options={
+//       method:"DELETE",
+//       headers:header
+//     }
+//     return fetch(`https://fullstackecommercewebsite.pythonanywhere.com/api/category/${id}/remove`,options)
+//   }
+// }
